9f02ba77a8f8460fbea4ff4896d99695647a3537
Ram Akuka <ramakuka@gmai.com>
2017-11-02 16:14:51 -0700

    removed unneeded vars

--- main.tf
+++ main.tf
@@ -12,13 +12,6 @@ data "aws_subnet_ids" "k8s" {
   vpc_id = "${data.external.default.result.vpc_id}"
 }
 
-resource "aws_route" "k8s" {
-  count                     = "${length(distinct(sort(data.aws_route_table.k8s.*.route_table_id)))}"
-  route_table_id            = "${element(distinct(sort(data.aws_route_table.k8s.*.route_table_id)), count.index)}"
-  destination_cidr_block    = "${data.aws_vpc_peering_connection.default.cidr_block}"
-  vpc_peering_connection_id = "${data.aws_vpc_peering_connection.default.id}"
-}
-
 data "aws_route_table" "k8s" {
   count     = "${length(distinct(sort(data.aws_subnet_ids.k8s.ids)))}"
   subnet_id = "${element(distinct(sort(data.aws_subnet_ids.k8s.ids)), count.index)}"
