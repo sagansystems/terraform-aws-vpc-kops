6d86fd1396027178b3687ebf3af5b987d41ce3b0
Ram Akuka <ramakuka@gmai.com>
2017-11-02 16:13:29 -0700

    removed unneeded vars

--- outputs.tf
+++ outputs.tf
@@ -5,3 +5,7 @@ output "k8s_vpc_id" {
 output "k8_nat_gateway" {
   value = "${data.external.default.aws_nat_gateway}"
 }
+
+output "vpc_id" {
+  value = "${data.external.default.result.vpc_id}"
+}
--- variables.tf
+++ variables.tf
@@ -1,7 +1,2 @@
 variable "cluster" {}
 
-variable "backing_services_vpc_id" {}
-
-variable "route_tables" {
-  type = "list"
-}
