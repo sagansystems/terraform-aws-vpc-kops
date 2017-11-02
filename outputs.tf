79d9322d0ab84d2220b0c163c9a210b9683f2ecb
Ram Akuka <ramakuka@gmai.com>
2017-11-02 16:40:09 -0700

    typo

--- outputs.tf
+++ outputs.tf
@@ -2,6 +2,6 @@ output "k8s_vpc_id" {
   value = "${data.external.default.result.vpc_id}"
 }
 
-output "k8_nat_gateway_public_ips" {
+output "k8s_nat_gateway_public_ips" {
   value = "${data.external.default.aws_nat_gateway.*.public_ip}"
 }
