faacde8e7a8ba522447d5e85870463fa4bd51279
Daren Desjardins <darend@gmail.com>
2017-11-15 21:07:42 -0800

    Add security groups

--- main.tf
+++ main.tf
@@ -8,3 +8,33 @@ data "aws_vpc" "kops" {
 data "aws_subnet_ids" "kops" {
   vpc_id = "${data.aws_vpc.kops.id}"
 }
+
+data "aws_security_group" "bastions" {
+  vpc_id = "${data.aws_vpc.kops.id}"
+  name   = "bastion.${var.dns_zone}"
+
+  tags {
+    Name              = "bastion.${var.dns_zone}"
+    KubernetesCluster = "${var.dns_zone}"
+  }
+}
+
+data "aws_security_group" "masters" {
+  vpc_id = "${data.aws_vpc.kops.id}"
+  name   = "masters.${var.dns_zone}"
+
+  tags {
+    Name              = "masters.${var.dns_zone}"
+    KubernetesCluster = "${var.dns_zone}"
+  }
+}
+
+data "aws_security_group" "nodes" {
+  vpc_id = "${data.aws_vpc.kops.id}"
+  name   = "nodes.${var.dns_zone}"
+
+  tags {
+    Name              = "nodes.${var.dns_zone}"
+    KubernetesCluster = "${var.dns_zone}"
+  }
+}
