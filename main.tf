b9b4e385d32c895c70b448372539bc0e386d934c
Daren Desjardins <daren@gladly.com>
2020-12-17 10:15:08 -0800

    Terraform 0.13 [ch81587] (#13)

--- main.tf
+++ main.tf
@@ -3,13 +3,13 @@ data "aws_vpc" "kops" {
 
   filter {
     name   = "tag:Name"
-    values = ["${var.dns_zone}"]
+    values = [var.dns_zone]
   }
 }
 
 data "aws_subnet_ids" "private" {
   count  = var.enabled ? 1 : 0
-  vpc_id = "${data.aws_vpc.kops[count.index].id}"
+  vpc_id = data.aws_vpc.kops[count.index].id
 
   tags = {
     "kubernetes.io/role/internal-elb" = "1"
@@ -18,7 +18,7 @@ data "aws_subnet_ids" "private" {
 
 data "aws_subnet_ids" "utility" {
   count  = var.enabled ? 1 : 0
-  vpc_id = "${data.aws_vpc.kops[count.index].id}"
+  vpc_id = data.aws_vpc.kops[count.index].id
 
   tags = {
     "kubernetes.io/role/elb" = "1"
@@ -27,33 +27,33 @@ data "aws_subnet_ids" "utility" {
 
 data "aws_security_group" "bastion" {
   count  = var.enabled ? 1 : 0
-  vpc_id = "${data.aws_vpc.kops[count.index].id}"
+  vpc_id = data.aws_vpc.kops[count.index].id
   name   = "bastion.${var.dns_zone}"
 
   tags = {
     Name              = "bastion.${var.dns_zone}"
-    KubernetesCluster = "${var.dns_zone}"
+    KubernetesCluster = var.dns_zone
   }
 }
 
 data "aws_security_group" "masters" {
   count  = var.enabled ? 1 : 0
-  vpc_id = "${data.aws_vpc.kops[count.index].id}"
+  vpc_id = data.aws_vpc.kops[count.index].id
   name   = "masters.${var.dns_zone}"
 
   tags = {
     Name              = "masters.${var.dns_zone}"
-    KubernetesCluster = "${var.dns_zone}"
+    KubernetesCluster = var.dns_zone
   }
 }
 
 data "aws_security_group" "nodes" {
   count  = var.enabled ? 1 : 0
-  vpc_id = "${data.aws_vpc.kops[count.index].id}"
+  vpc_id = data.aws_vpc.kops[count.index].id
   name   = "nodes.${var.dns_zone}"
 
   tags = {
     Name              = "nodes.${var.dns_zone}"
-    KubernetesCluster = "${var.dns_zone}"
+    KubernetesCluster = var.dns_zone
   }
 }
--- variables.tf
+++ variables.tf
@@ -1,7 +1,9 @@
 variable "enabled" {
-  default = "true"
+  type    = bool
+  default = true
 }
 
 variable "dns_zone" {
   description = "Name of the kops DNS zone - it usually set up by galaxy as TF_VAR_dns_zone"
+  type        = string
 }
